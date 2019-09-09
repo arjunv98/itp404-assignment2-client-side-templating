8ab5c2163915d04618de76edf7dc5b66a0115d8e
Arjun Viswanathan <arjunvis@usc.edu>
2019-09-08 18:26:44 -0700

    Implemented Handlebars

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-$('.search-bar button').on('click', function() {
+const postsTemplate = Handlebars.compile(
+  document.getElementById('posts-template').innerHTML
+);
+
+const headerTemplate = Handlebars.compile(
+  document.getElementById('header-template').innerHTML
+);
+
+Handlebars.registerHelper('format-num', function(num) {
+  return num.toLocaleString();
+});
+
+$('.search-bar button').on('click', async function(e) {
+  e.preventDefault();
   $('#results').html('<div class="loader">Loading...</div>');
 
   let searchTerm = $('.search-bar input').val();
   let cleanSearchTerm = DOMPurify.sanitize(searchTerm);
   let url = "https://www.reddit.com/r/" + cleanSearchTerm + ".json";
 
-  let promise = $.ajax({
-    type: 'GET',
-    url: url
-  });
-
-  promise.then(function(posts) {
-    posts = posts.data.children;
-    let fragment = document.createDocumentFragment();
-
-    posts.forEach(function(post) {
-      let div = document.createElement('div');
-      div.className = 'post';
-
-      let title = document.createElement('a');
-      title.href = post.data.url;
-      title.innerText = post.data.title;
-
-      let author = document.createElement('p');
-      author.innerText = 'Author: ' + post.data.author;
-
-      let score = document.createElement('p');
-      score.innerText = post.data.score + ' upvotes';
-
-      div.appendChild(title);
-      div.appendChild(author);
-      div.appendChild(score);
-      fragment.append(div);
+  try {
+    let posts = await $.ajax({
+      type: 'GET',
+      url: url
     });
+    
+    posts = posts.data.children;
+    let firstPost = posts[0];
 
-    let sanitizedHtml = DOMPurify.sanitize(fragment);
-    $('#results').html(sanitizedHtml);
-  });
+    let sanitizedHeader = headerTemplate({ firstPost });
+    let sanitizedPosts = postsTemplate({ posts });
+
+    $('#header').html(sanitizedHeader);
+    $('#results').html(sanitizedPosts); 
+  } catch (error) {
+    $('#results').html('Oops, something went wrong.')
+    console.log(error);
+  }
+});
+
+/* Can search by hitting return */
+$('.search-bar').on('keypress', function(e) {
+  if(e.which == 13) {
+    $('.search-bar button').click();
+  }
 });
